43e7ff022c4cd578e33e67c7013fde06e784b075
rijal <jeryzal333@gmail.com>
2022-07-14 20:00:53 +0700

    computed string

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,24 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ ninjaOne.name }} my age is {{ ninjaOne.age }}</p>
-    <button @click="handleClick">Click me</button>
-    <h1>Reactive</h1>
-    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <p>{{ name }}</p>
   </div>
 </template>
 
 <script>
-import { reactive, ref } from "@vue/reactivity";
+/* eslint-disable */ 
+import { computed, reactive, ref } from "@vue/reactivity";
 // @ is an alias to /src
 // disable-eslint
 
 export default {
   name: "Home",
   setup() {
-    const ninjaOne = ref({ name: "Rijal", age: 27 });
-    const ninjaTwo = reactive({ name: "Husen", age: 47 })
+    const name = computed(() => {
+      return "Rijal"
+    })
 
-    const handleClick = () => {
-      ninjaOne.value.age = 37;
-      // we dont ne access the .value here
-      // but we can't change the reactive when the data is primitive e.g = reactive("string")
-      ninjaTwo.age = 50;
-    };
-
-    return { ninjaOne, ninjaTwo, handleClick };
+    return { name };
   },
 };
 </script>
