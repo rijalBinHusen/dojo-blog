a9b322278da4a2603bb62ac97b0d812b69c12d5f
rijal <jeryzal333@gmail.com>
2022-07-14 19:39:37 +0700

    template refs

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="home">
     Home
-    <p>My name is {{ name }} my age is {{ age }}</p>
+    <p ref="p">My name is {{ name }} my age is {{ age }}</p>
     <button @click="handleClick">Click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 // @ is an alias to /src
 
 export default {
   name: "Home",
   setup() {
+
+    const p = ref(null)
+
     let name = "Rijal";
     let age = 27;
 
     const handleClick = () => {
-      console.log("clicked")
+      p.value.classList.add("testt")
+      p.value.textContent = "Hello world"
+      console.log(p, p.value)
     };
 
-    return { name: name, age: age, handleClick }
+    return { name, age, handleClick, p }
 
   },
 }
