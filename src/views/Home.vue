84977b62c0659a21ab673af4947a5db50216781f
rijal <jeryzal333@gmail.com>
2022-07-15 08:20:47 +0700

    reusable composition functions (composable)

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -13,33 +13,17 @@
 
 <script>
 /* eslint-disable */ 
-import { computed, reactive, ref } from "@vue/reactivity";
-import { watch, watchEffect } from '@vue/runtime-core';
 import PostLists from "../components/PostLists.vue"
+import getPosts from "../composables/getPosts"
 
 export default {
   name: "Home",
   components: { PostLists },
   setup() {
-    const posts = ref([])
-    const error = ref(null)
-
-    const load = async () => {
-      try {
-        let data = await fetch("http://localhost:3000/posts")
-        if(!data.ok) {
-          throw Error("no data available")
-        }
-        posts.value = await data.json()
-      }
-      catch(err) {
-        error.value  = err.message
-        // console.log(error.value)
-      }
-    }
+    const { posts, error, load } = getPosts()
 
     load()
-
+    
     return { posts, error };
   },
 };
