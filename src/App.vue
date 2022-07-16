0bcc86434347b9822128815dd9d417a7cba0ae24
rijal <jeryzal333@gmail.com>
2022-07-17 05:21:48 +0700

    routing navbar

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
+  <Navbar />
   <router-view />
 </template>
+
+<script>
+import Navbar from "./components/Navbar.vue"
+export default {
+  setup() {
+    
+  },
+  components: { Navbar }
+}
+</script>
 
 <style>
 #app {
